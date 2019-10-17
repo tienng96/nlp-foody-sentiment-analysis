3cb3791746cb43c3d3206fde136d3572aef9c2ff
hieuvt <hitvn-hieuai>
2019-10-17 12:28:03 +0700

    updated

--- processor/processor.py
+++ processor/processor.py
@@ -50,24 +50,24 @@ def tokenize(text, stopwords):
     text_segmented = [elem for elem in text_segmented if elem not in stopwords]
     return ' '.join(text_segmented)
 
-def vectorize(reviews):
-    if args.is_first == 1:
-        vectorizer = TfidfVectorizer(min_df=5,max_features=50000, sublinear_tf=True)
-        vectorizer.fit(reviews)
-        pickle_out = open("../model/vectorizer.pickle", "wb")
-        pickle.dump(vectorizer, pickle_out)
-        pickle_out.close()
-    else:
-        with open(r"../model/vectorizer.pickle","rb") as input_file:
-            vectorizer = pickle.load(input_file)
-    X_array = vectorizer.transform(reviews).toarray()
-    print("X_array.shape:",X_array.shape)
-    return X_array
+# def vectorize(reviews):
+#     if args.is_first == 1:
+#         vectorizer = TfidfVectorizer(min_df=5,max_features=50000, sublinear_tf=True)
+#         vectorizer.fit(reviews)
+#         pickle_out = open("../model/vectorizer.pickle", "wb")
+#         pickle.dump(vectorizer, pickle_out)
+#         pickle_out.close()
+#     else:
+#         with open(r"../model/vectorizer.pickle","rb") as input_file:
+#             vectorizer = pickle.load(input_file)
+#     X_array = vectorizer.transform(reviews).toarray()
+#     print("X_array.shape:",X_array.shape)
+#     return X_array
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-first', dest='is_first', type=int, help='dump_file_pickle')
-    args = parser.parse_args()
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument('-first', dest='is_first', type=int, help='dump_file_pickle')
+    # args = parser.parse_args()
 
     for file in os.listdir(path_json):
         path_file = os.path.join(path_json, file)
@@ -81,7 +81,7 @@ if __name__ == '__main__':
     df = df[df.score.isnull() == False].reset_index(drop=True)
     df = processor_score(df)
     print("Number_comment:",len(df['comment'].values))
-
+    print("Processing stopwords...")
     with open("./vietnamese-stopwords-dash.txt", encoding="utf8") as input_file:
         x = input_file.readlines()
         removetable = str.maketrans('', '', '\n')
@@ -97,31 +97,34 @@ if __name__ == '__main__':
         reviews.append(temp_text)
     for i, score in enumerate(df['score']):
         reviews[i] = reviews[i] + ',' + '{}'.format(score)
+    print("Number comment POS:", len(df[df['score'] == 1]))
+    print("Number comment NEG:", len(df[df['score'] == 0]))
     #Write file csv
+    print('Save file csv...')
     with open('../data/comment_processing.csv','w') as out_file:
         writer = csv.writer(out_file)
         # writer.writerow(('comment','score'))
         for text in reviews:
             row = [c.strip() for c in text.strip(', ').split(',')]
             writer.writerow(row)
-    #Load file csv
-    X_comment = []
-    X_label = []
-    with open('../data/comment_processing.csv','r') as input_file:
-        reader_file = csv.reader(input_file)
-        for row in reader_file:
-            X_comment.append(row[0])
-            X_label.append(row[1])
-    # TF-IDF
-    X_comment = vectorize(X_comment)
-    X_label = X_label
-    print("Number comment POS:",len(df[df['score'] == 1]))
-    print("Number comment NEG:", len(df[df['score'] == 0]))
-    # Handling imbalanced Data - Under sampling
-    X_label = np.asarray(X_label)
-    X_comment = np.asarray(X_comment)
-    nm = NearMiss(random_state=42)
-    X_res,Y_res = nm.fit_sample(X_comment,X_label)
-    print("comment_process_balanced:",X_res.shape)
-    print("score_process_balanced:",Y_res.shape)
-    train.train(X_res,Y_res)
+    print('Congradulation!Saved success!')
+    # #Load file csv
+    # X_comment = []
+    # X_label = []
+    # with open('../data/comment_processing.csv','r') as input_file:
+    #     reader_file = csv.reader(input_file)
+    #     for row in reader_file:
+    #         X_comment.append(row[0])
+    #         X_label.append(row[1])
+    # # TF-IDF
+    # X_comment = vectorize(X_comment)
+    # X_label = X_label
+    #
+    # # Handling imbalanced Data - Under sampling
+    # X_label = np.asarray(X_label)
+    # X_comment = np.asarray(X_comment)
+    # nm = NearMiss(random_state=42)
+    # X_res,Y_res = nm.fit_sample(X_comment,X_label)
+    # print("comment_process_balanced:",X_res.shape)
+    # print("score_process_balanced:",Y_res.shape)
+    # train.train(X_res,Y_res)
